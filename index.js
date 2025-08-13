db56c6013b9267ddb51697516cf0320dfaefbe83
Will They <juliettemajid@gmail.com>
2025-08-13 21:28:03 +0100

    :sparkles: add memo peace out !!!

--- index.js
+++ index.js
@@ -1,28 +1,73 @@
 
 
-let mykey = document.getElementById("mykey");
-let myinput = document.getElementById("myinput");
-let mybtn = document.getElementById("mybtn");
-let memo1 = document.getElementById("memo1");
-let key1 = document.getElementById("key1");
-let btn1 = document.getElementById("btn1");
-let ctrlv = document.getElementById("ctrlv")
 let mytext = document.getElementById("mytext")
 let btn2 = document.getElementById("btn2")
 let memo2 = document.getElementById("memo2")
+let btn1 = document.getElementById("btn1")
+let memo1 = document.getElementById("memo1")
+let plus = document.getElementById("plus")
+let count = 1;
 
-mybtn.onclick = function(){
-    localStorage.setItem(mykey.value,myinput.value);
-}
+//to reset counter:
+//localStorage.setItem("count",0); 
+count = Number(localStorage.getItem("count"));
+
 
 btn1.onclick = function(){
-    memo1.textContent = localStorage.getItem(key1.value);
-}
-
-ctrlv.onclick = function(){
-    localStorage.setItem("key",mytext.value)
+    localStorage.setItem("memo1", mytext.value);
+    memo1.textContent = localStorage.getItem("memo1");
+    console.log(localStorage.getItem("memo1"));
 }
 
 btn2.onclick = function(){
-    memo2.textContent = localStorage.getItem("key")
+    localStorage.setItem("memo2", mytext.value);
+    memo2.textContent = localStorage.getItem("memo2");
+    console.log(localStorage.getItem("memo2"));
 }
+
+
+plus.onclick = function(){
+    console.log("hello we up run this city yuh")
+    count += 1
+    console.log(count)
+    localStorage.setItem("count",count)
+    newMemo()
+}
+
+memo2.textContent = localStorage.getItem("memo2");
+memo1.textContent = localStorage.getItem("memo1");
+
+
+//trying to add new memos
+let label = document.createElement("label");
+let btn = document.createElement("button");
+
+label.setAttribute("class","memoH");
+btn.setAttribute("class", "paste");
+
+
+newMemo = function(){
+    console.log("neha waz here")
+    let para = document.createElement("p");
+    console.log(para);
+    let section = document.createElement("section");
+    section.setAttribute("id",`section${count+2}`);
+    para.setAttribute("id",`memo${count+2}`);
+    console.log(para)
+    let newsection = document.getElementById(`section${count+2}`);
+    let newpara = document.getElementById(`memo${count+2}`);
+    newpara.textContent(`memo${count+2}`);
+    document.body.insertBefore(section,plus);
+    newsection.appendChild(para);
+}
+
+newMemo();
+
+
+//for(let i = 1; i <= count; ++i)
+//    newmemo();
+
+
+
+
+console.log(count)
