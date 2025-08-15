6c9ce94610c4d7a226fc9b33912095fa2d6bfbd0
Will They <juliettemajid@gmail.com>
2025-08-15 16:42:38 +0100

    added delete button

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-
-
 let mytext = document.getElementById("mytext");
 let plus = document.getElementById("plus");
 let minus = document.getElementById("minus");
 let erase = document.getElementById("erase");
 let paste = document.getElementsByClassName("paste");
-let count = 1;
+let memoList = [];
 
 
-//to reset counter:
-//localStorage.setItem("count",0); 
-count = Number(localStorage.getItem("count"));
+function reset(){
+    memoList = []
+    storeMemo()
+}
+//reset()
+
+memoList = accessMemo();
+function storeMemo(){
+    localStorage.setItem("memoList",JSON.stringify(memoList));
+}
+function accessMemo(){
+    return JSON.parse(localStorage.getItem("memoList"));
+}
+console.log(memoList);
+
 
 /*trying to make delete button:
 idea is instead of memos stored individ, store as a list, and memo n displays the nth entry in the list
@@ -21,73 +31,92 @@ side note: have load memo contents as separate function to load memo blocks
 
 //trying to add new memos
 
-newMemo = function(i){
-    console.log(`newMemo #${i}`);
+function createMemo(i){
+    //create elements
     let section = document.createElement("section");
     let label = document.createElement("label");
     let button = document.createElement("button");
     let para = document.createElement("p");
-
+    let minus = document.createElement("button");
+    //set attributes
+    section.setAttribute("id",`section${i}`)
     section.setAttribute("class","flow");
     para.setAttribute("id",`memo${i}`);
     label.setAttribute("class", "memoH");
     button.setAttribute("id",`btn${i}`);
     button.setAttribute(`class`,`paste`);
-
-    document.body.insertBefore(section,plus);
+    button.setAttribute("onclick",`display(${i})`)
     label.textContent = `memo#${i} ~ `;
+    minus.textContent = "x"
+    minus.setAttribute("class","minus")
+    minus.setAttribute("onclick",`minusMemo(${i})`)
+    //assemble
+    document.body.insertBefore(section,plus);
+    section.appendChild(minus)
     section.appendChild(label);
     section.appendChild(button);
     section.appendChild(para);
+}
+
+newMemo = function(i){
+    console.log(`load newMemo #${i}`);
+    createMemo(i);
+    memoList.push("");
+    storeMemo();
+    console.log(memoList);
     
 }
 
 function load(){
 let i;
-for(let i = 1; i <= count; ++i) {
-    console.log(`oldMemo #${i}`);
-    let section = document.createElement("section");
-    let label = document.createElement("label");
-    let button = document.createElement("button");
-    let para = document.createElement("p");
-
-    section.setAttribute("class","flow");
-    para.setAttribute("id",`memo${i}`);
-    label.setAttribute("class", "memoH");
-    button.setAttribute("id",`btn${i}`);
-    button.setAttribute("class", "paste");
-    button.setAttribute("onclick",`display(${i})`)
-
-    document.body.insertBefore(section,plus);
-    label.textContent = `memo#${i} ~ `;
-    section.appendChild(label);
-    section.appendChild(button);
-    section.appendChild(para);
-    document.getElementById(`memo${i}`).textContent = localStorage.getItem(`memo${i}`);
+for(let i = 1; i <= memoList.length; ++i) {
+    console.log(`load oldMemo #${i}`);
+    createMemo(i);
+    document.getElementById(`memo${i}`).textContent = accessMemo()[i-1];
 }
 }
 load()
 
 
-plus.onclick = function(){
-    count += 1
-    localStorage.setItem("count",count)
-    newMemo(paste.length + 1)
-    console.log(`new count = ${count}`)
-}
-erase.onclick = function(){
-    console.log("erase")
-    for(let k=1;k<=count;k++) {
-        localStorage.setItem(`memo${k}`,"");
-        document.getElementById(`memo${k}`).textContent = "";
+function loadMemos(){
+    for(let k=0; k<memoList.length; k++) {
+        document.getElementById(`memo${k+1}`).textContent = memoList[k]
     }
 }
 
-function display(n){
-    console.log(n)
-    localStorage.setItem(`memo${n}`,mytext.value);
-    document.getElementById(`memo${n}`).textContent = localStorage.getItem(`memo${n}`);
+plus.onclick = function(){
+    newMemo(paste.length + 1)
+    console.log("plus clicked")
+}
+erase.onclick = function(){
+    console.log("erase")
+    for(let k=0; k<memoList.length; k++) {
+        memoList[k] = ""
+        document.getElementById(`memo${k+1}`).textContent = memoList[k]
+    }
+    storeMemo()
 }
 
-console.log(`no of memos is ${paste.length}`)
-console.log(`initial count = ${count}`)
+function minusMemo(i){
+    document.getElementById(`section${memoList.length}`).remove();
+    let front = memoList.slice(0,i-1);
+    let end = memoList.slice(i,memoList.length);
+    console.log(i);
+    console.log(front);
+    console.log(end);
+    memoList = front.concat(end);
+    console.log(memoList)
+    storeMemo();
+    loadMemos();
+
+}
+
+function display(n){
+    console.log(`paste on memo#${n}`);
+    memoList[n-1] = mytext.value;
+    storeMemo();
+    document.getElementById(`memo${n}`).textContent = memoList[n-1]
+    console.log(memoList)
+}
+
+console.log(`no of memos = ${memoList.length}`)
